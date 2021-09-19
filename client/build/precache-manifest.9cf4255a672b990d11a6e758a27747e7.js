07c102ee9eee4d118e06e1167af2388f2663d1ff
dillon walsh <dillonwalsh93@gmail.com>
2021-09-19 12:14:47 -0400

    added cart and cart item components

---
+++ client/build/precache-manifest.9cf4255a672b990d11a6e758a27747e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe5abc8cda1ddaa6d954e0e27bd3a4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a67bba88026e1a0b411",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "e9005f624c6a224fcc45",
+    "url": "/static/js/2.d3958a85.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.d3958a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a67bba88026e1a0b411",
+    "url": "/static/js/main.40d59d21.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
